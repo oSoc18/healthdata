471dd611ce8d9cc90979f74b2c28801cefd7a51d
Michiel Leyman <michiel.leyman@gmail.com>
2018-07-11 12:39:15 +0200

    Update eslint and parser versions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './assets/css/index.css';
-
-/* eslint-disable react/jsx-filename-extension */
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
