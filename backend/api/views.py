131126525a98c04afbcbb324e4bd2f7fd8f431d0
Pizzibee <sushilghambir@gmail.com>
2018-07-12 15:14:36 +0200

    Correction of number of beds bug

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 import os
@@ -11,10 +10,17 @@ def importHospitals(request):
     csvFile = open(os.path.join(settings.BASE_DIR, 'api', 'csvFiles', 'hospitals.csv'))
     reader = csv.DictReader(csvFile)
     for row in reader:
-        if not isinstance(row['beds'], int):
+        if not isInt(row['beds']):
             #if number of beds not provided
             p = Hospital(name=row['name'], latitude=row['lat'], longitude=row['long'], nbBeds=0)
         else:
             p = Hospital(name=row['name'], latitude=row['lat'], longitude=row['long'], nbBeds=int(row['beds']))
         p.save()
     return HttpResponse("<h1>Imported succesfully</h1>")
+
+def isInt(value):
+    try:
+        int(value)
+        return True
+    except:
+        return False
