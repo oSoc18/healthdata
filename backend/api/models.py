630d9e1d4a3d66029b5eea387c26878e86547584
Pizzibee <sushilghambir@gmail.com>
2018-07-12 10:51:54 +0200

    Hospital models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Hospitals(models.Model):
+    name = models.CharField(max_length = 500)
+    latitude = models.CharField(max_length = 250)
+    longitude = models.CharField(max_length = 250)
+    nbBeds = models.IntegerField()
