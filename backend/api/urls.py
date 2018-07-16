88c34ab4bbc33ab1138b116a81a0684d0116a440
Niels Vandekeybus <progster@gmail.com>
2018-07-17 00:16:15 +0200

    initial etl support
    added 2 etls:
    - 1 to import the hospital csv (based on the csv import from sushil)
    - 1 EXAMPLE downloading excell files and storing them locally (should still parse them etc...)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,6 @@ from api import views
 
 
 urlpatterns = [
-    url('importHospitals', views.importHospitals),
     url('hospitals/(?P<pk>[0-9]+)/$', views.hospital_detail),
     url('hospitals', views.hospital_list),
 ]
