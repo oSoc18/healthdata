40d54a652d6c448707665234b368f57a656b28ff
Pizzibee <sushilghambir@gmail.com>
2018-07-16 13:52:18 +0200

    Api with hospital finished

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     url('importHospitals', views.importHospitals),
+    url('hospitalDetail/(?P<pk>[0-9]+)/', views.hospital_detail),
+    url('hospitals', views.hospital_list),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from django.conf import settings
 import os
 import csv
 from api.models import Hospital
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from api.models import Hospital
+from api.serializers import HospitalSerializer
 
 # Create your views here.
 def importHospitals(request):
@@ -24,3 +31,16 @@ def isInt(value):
         return True
     except:
         return False
+
+def hospital_list(request):
+    hospitals = Hospital.objects.all()
+    serializer = HospitalSerializer(hospitals, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def hospital_detail(request, pk):
+    try:
+        hospital = Hospital.objects.get(pk=pk)
+    except Hospital.DoesNotExist:
+        raise Http404("Hospital not found")
+    serializer = HospitalSerializer(hospital)
+    return JsonResponse(serializer.data)
