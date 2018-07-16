73203e7d4e932e51c50b8e237718962eb11b8b50
Niels V <nvdk@users.noreply.github.com>
2018-07-16 21:25:47 +0200

    Add cors support (#61)

--- backend/healthBackend/settings.py
+++ backend/healthBackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*'] # windows 10 support, remove for production!
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django_admin_index',
     'ordered_model',
     'django.contrib.admin',
@@ -44,10 +45,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -127,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS configuration
+CORS_ORIGIN_ALLOW_ALL = True
