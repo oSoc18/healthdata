b16d2d3cd95d44cdc8de8bff8f19340bd107209d
Pizzibee <sushilghambir@gmail.com>
2018-07-17 15:48:49 +0200

    Xls to csv conversion

--- backend/api/management/commands/historicaldata.py
+++ backend/api/management/commands/historicaldata.py
@@ -1,6 +1,9 @@
 import bonobo
 import requests
 import os
+import xlrd
+import unicodecsv
+from django.conf import settings
 from bonobo.contrib.django import ETLCommand
 from django.conf import settings
 
@@ -20,11 +23,24 @@ def download_excels(url):
         for chunk in r.iter_content(chunk_size=1024):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
-    return local_filename
+    yield local_filename
+
+def convert_excels(filename):
+    wb = xlrd.open_workbook(filename)
+    sh = wb.sheet_by_index(0)
+
+    fh = open(os.path.join(settings.BASE_DIR, 'api', 'source-data', "hospitalsDetailed.csv"), "wb")
+    csv_out = unicodecsv.writer(fh, encoding='utf-8')
+
+    for row_number in range(sh.nrows):
+        csv_out.writerow(sh.row_values(row_number))
+
+    fh.close()
+
 # https://fair-acc.healthdata.be/api/3/action/group_package_show?id=469baf11-ddd1-4c30-9ad3-a21a7d0f7397
 # returns all historical data
 class Command(ETLCommand):
     def get_graph(self, **options):
         graph = bonobo.Graph()
-        graph.add_chain(get_excel_urls, bonobo.Limit(50), download_excels, bonobo.PrettyPrinter())
+        graph.add_chain(get_excel_urls, bonobo.Limit(10), download_excels, bonobo.PrettyPrinter(), convert_excels)
         return graph
