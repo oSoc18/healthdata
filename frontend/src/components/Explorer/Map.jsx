b4df87278724d18d1aab73642cc29654b644edec
Michiel Leyman <MichielLeyman@users.noreply.github.com>
2018-07-21 13:45:44 +0200

    Add bounding box around Belgium (#101)
    * Add @turf/bbox
    
    * Make Map stateful
    
    * Set max bounds on map
    
    * Update style - restrict zoom levels
    
    * Use healthstory red

--- frontend/src/components/Explorer/Map.jsx
+++ frontend/src/components/Explorer/Map.jsx
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import { observer } from 'mobx-react';
+import bbox from '@turf/bbox';
 import HospitalMarker from './HospitalMarker';
 import provincesGeoJSON from '../../assets/data/be-provinces.geo.json';
 
 import 'leaflet/dist/leaflet.css';
 import '../../assets/css/explorer/map.css';
 
-const MapLeaflet = ({ store }) => {
-  const { hospitals } = store;
-  return (
-    <Map className="leaflet-container" center={[50.52, 4.3517]} zoom={8} dragging={false} zoomControl={false} scrollWheelZoom={false}>
-      <TileLayer
-        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-      />
-      <GeoJSON
-        data={provincesGeoJSON}
-        style={{
-          fillColor: 'yellow',
-          weight: 3,
-          opacity: 0.65
-        }}
-      />
-      {
-        hospitals.map(hospital => <HospitalMarker key={hospital.id} hospital={hospital} />)
-      }
-    </Map>
-  );
-};
+class MapLeaflet extends Component {
+  constructor(props) {
+    super(props);
+    const bboxArray = bbox(provincesGeoJSON);
+    const corner1 = [bboxArray[1], bboxArray[0]];
+    const corner2 = [bboxArray[3], bboxArray[2]];
+    this.state = {
+      bounds: [corner1, corner2]
+    };
+  }
+  render() {
+    const { hospitals } = this.props.store;
+    return (
+      <Map
+        className="leaflet-container"
+        center={[50.52, 4.3517]}
+        zoom={8}
+        minZoom={8}
+        maxZoom={17}
+        maxBounds={this.state.bounds}
+        maxBoundsViscosity={0.5}
+      >
+        <TileLayer
+          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+        />
+        <GeoJSON
+          data={provincesGeoJSON}
+          style={{
+            color: '#FF6464',
+            fillColor: 'transparent',
+            weight: 1,
+            opacity: 0.6
+          }}
+        />
+        {
+          hospitals.map(hospital => <HospitalMarker key={hospital.id} hospital={hospital} />)
+        }
+      </Map>
+    );
+  }
+}
 
 export default observer(MapLeaflet);
