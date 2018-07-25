89d1b5141cbb86270d6231ca245c0f4885fea521
Thomas Duvivier <33236839+thduvivier@users.noreply.github.com>
2018-07-25 13:19:59 +0200

    Add picture and content in Comparison Belgium

--- frontend/src/components/Journey/comparisons/ComparisonBelgium.jsx
+++ frontend/src/components/Journey/comparisons/ComparisonBelgium.jsx
@@ -19,9 +19,10 @@ class ComparisonBelgium extends React.Component {
         <div className="journey_content">
           <h1>Comparison over Belgium</h1>
           <p>
-            Meanwhile, Belgium counts 703,700 people with depression, which is more than the inhabitants of Antwerp AND Namur combined (show city of antwerp and Namur).
-            (0.062*11,350,000=703,700 people in Belgium in 2017; Antwerp = 520,000; Namur=110,000 -> We can say “more than the inhabitants of Antwerp and Namur combined”).
+            In 2017, Belgium counted <span className="bold red">703,700</span> individuals with symptoms of depressive disorder, which is more than the inhabitants of Antwerp and Namur combined.
+            {/* (0.062*11,350,000=703,700 people in Belgium in 2017; Antwerp = 520,000; Namur=110,000 -> We can say “more than the inhabitants of Antwerp and Namur combined”). */}
           </p>
+          <div className="namurAntwerp"></div>
           <p>
             <button type="button" className="redButtonLink" onClick={() => this.props.prev()}>
               <i className="fa fa-angle-left bold"></i> Go back
